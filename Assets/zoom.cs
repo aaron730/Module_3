c8c914e3e10de6984305e8054e160dda5e7b8bfc
aaron730 <55248680+aaron730@users.noreply.github.com>
2021-02-19 14:40:21 -0600

    zoom

---
+++ Assets/zoom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zoom : MonoBehaviour
+{
+
+    public float acceleration;
+    public float maxSpeed;
+
+    private float curSpeed = 0;
+
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * curSpeed);
+
+        curSpeed += acceleration;
+
+        if (curSpeed > maxSpeed)
+            curSpeed = maxSpeed;
+
+    }
+}
+
+
